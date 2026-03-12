5797cd76a596b28dd74b093321c952ff241c04b2
ambalavanan01 <nikeshshivan12@gmail.com>
2026-03-12 18:36:08 +0530

    created android app

---
+++ android/app/src/main/java/com/ambalavanan/portfolio/MainActivity.java
@@ -0,0 +1,5 @@
+package com.ambalavanan.portfolio;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ambalavanan.portfolio',
+  appName: 'Ambalavanan Portfolio',
+  webDir: 'dist'
+};
+
+export default config;
